fccdecaa4eb16bbe4f6c526cddc9a8ba1594ce2e
Emre kavcakar <kavcakaremre@gmail.com>
2023-03-04 21:58:56 -0500

    legend

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,19 @@
+import Container from "react-bootstrap/esm/Container"
+import {data} from "../helpers/data"
+import PlayerCard from "./PlayerCard";
+
+
+const CardContainer = () => {
+    return(
+        <Container className="card-container rounded-4 my-4 p-3">
+         {data.map((player) =>(
+            <PlayerCard player = {player} />
+
+         )
+            
+            )}
+        </Container>
+    )
+}
+
+export default CardContainer
---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,15 @@
+import CardContainer from "./CardContainer"
+import Card from 'react-bootstrap/Card';
+const PlayerCard = () => {
+    return(
+    <Card>
+    <Card.Img variant ="top" src="holder.js/100px180?text=Image cap />"
+        <Card.Footer>
+        <Card.Title>Card Link</Card.Title>
+        </Card.Footer>
+    </Card>
+         
+    )
+}
+
+export default PlayerCard
