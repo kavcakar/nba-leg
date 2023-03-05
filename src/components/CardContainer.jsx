64295c98c0aa2c5d440448334c6e4a1548e78f99
Emre kavcakar <kavcakaremre@gmail.com>
2023-03-04 22:40:25 -0500

    legend4

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,4 +1,6 @@
 import Container from "react-bootstrap/esm/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import {data} from "../helpers/data"
 import PlayerCard from "./PlayerCard";
 
@@ -6,12 +8,15 @@ import PlayerCard from "./PlayerCard";
 const CardContainer = () => {
     return(
         <Container className="card-container rounded-4 my-4 p-3">
-         {data.map((player) =>(
-            <PlayerCard {...player} />
-
-         )
-            
-            )}
+         <Row className="g-3 juatify-content-center">
+         {data.map((player, i) =>(
+             <Col md={6} lg={4} xl={3} key={i}>
+             <PlayerCard {...player} />
+             </Col>
+             ))}
+             
+             
+             </Row>
         </Container>
     )
 }
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -2,8 +2,8 @@ import CardContainer from "./CardContainer"
 import Card from 'react-bootstrap/Card';
 const PlayerCard = ({name, img, statistics}) => {
     return(
-    <Card>
-    <Card.Img variant ="top" src={img}/>
+    <Card className="rounded-2 m-auto player-card" role="button">
+    <Card.Img variant ="top" src={img} className=" player-logo"/>
         <Card.Footer>
         <Card.Title>Card Link</Card.Title>
         </Card.Footer>
